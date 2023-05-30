a36f81667a12bef97780a24bbc9f44b519aa1ae5
John-Davidson-8 <jodo828@gmail.com>
2023-05-30 11:02:34 +0100

    01 challenge

---
+++ challenges/01-vars.js
@@ -0,0 +1,17 @@
+/*
+## Variables #1
+
+1. Create "firstName" and "last_name" variables.
+2. Assign your values.
+3. Create "address" variable and assign "main street" value to it.
+4. Re-assign address to "first street" later.
+5.log all values in the console
+
+*/
+const firstName = "john";
+const last_name = "davidson";
+
+let address = "main street";
+address = "first street";
+
+console.log(firstName, last_name, address);
