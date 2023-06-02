377d7a55b4e65ef27c1621824481c12271e9e656
John-Davidson-8 <jodo828@gmail.com>
2023-06-02 16:45:33 +0100

    challenge 6 objects

--- challenges/06-objects.js
+++ challenges/06-objects.js
@@ -18,7 +18,43 @@ const person = {
 
 const age = person.age;
 console.log(age);
-person.name = "bob";
+person.name = "bob"; // this is an example of a dot.notation.
 console.log(person.name);
 console.log(person.siblings[2]);
 person.greeting();
+
+/* 
+## Challenge 06 Objects 
+
+1.Create car object
+2. Add make, model, year, colors (array), hybrid (boolean) keys
+3. Add two methods (drive and stop)
+4. In the function body setup log with random text
+5. Log make
+6. Log first color
+7. Invoke both methods
+
+*/
+
+const car = {
+  make: "ford",
+  model: "focus",
+  year: 2010,
+  color: ["blue", "red", "green", "orange"],
+  hybrid: false,
+  //the following code below is two methods (old way and new way)
+  drive: function () {
+    console.log("driving...");
+  },
+  stop() {
+    console.log("stopped!!!");
+  },
+};
+
+console.log(car.make);
+console.log(car.color[1]);
+
+// below is code to invoke both methods with dot notation
+
+car.drive();
+car.stop();
