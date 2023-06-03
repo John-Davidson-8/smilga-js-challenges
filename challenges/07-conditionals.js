d4965e97c5e06787da38b0216223b58376cc26e5
John-Davidson-8 <jodo828@gmail.com>
2023-06-03 15:53:46 +0100

    conditionals

---
+++ challenges/07-conditionals.js
@@ -0,0 +1,15 @@
+// Conditional Statements
+// >, <, >=, <=, ==, ===, !=, !==
+
+// example 1
+if (2 > 1) {
+  console.log("hello world");
+}
+
+// example 2
+const value = true;
+
+if (value) {
+  console.log("hi world");
+}
+console.log(typeof value);
