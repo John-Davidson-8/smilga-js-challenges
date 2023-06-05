c7edb57fe7482120bbefc7b26ba4a521c7502c0c
John-Davidson-8 <jodo828@gmail.com>
2023-06-05 20:33:08 +0100

    js conditionals

--- challenges/07-conditionals.js
+++ challenges/07-conditionals.js
@@ -2,14 +2,39 @@
 // >, <, >=, <=, ==, ===, !=, !==
 
 // example 1
-if (2 > 1) {
-  console.log("hello world");
-}
+// if (2 > 1) {
+//   console.log("hello world");
+// }
 
 // example 2
-const value = true;
+// const value = true;
 
-if (value) {
-  console.log("hi world");
-}
-console.log(typeof value);
+// if (value) {
+//   console.log("hi world");
+// }
+// console.log(typeof value);
+
+// example 3
+// const num1 = 4;
+// const num2 = 6;
+
+// if (num1 > num2) {
+//   console.log("first number is bigger than second");
+// } else {
+//   console.log("second number is bigger than first");
+// }
+
+// // example 4
+// const num1 = 6;
+// const num2 = 6;
+// const result = num1 >= num2;
+
+// if (num1 > num2) {
+//   console.log("first number is bigger than second");
+// } else if (result) {
+//   console.log("first number equal to second");
+// } else {
+//   console.log("second number is bigger than first");
+// }
+
+// example 5
